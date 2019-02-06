7edd6d95c895e7a111ba88f93cd3e791dce372e6
Taylor Williams <taylor@thewilliams.net>
2019-02-06 01:06:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function setThisWithCall(fn, thisValue, arg){
 function setThisWithApply(fn,thisValue,arg){
   return fn.apply(thisValue, arg)
 }
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  return thisValue.bind(functionToBeCopied)
+}
